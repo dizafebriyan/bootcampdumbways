bef4f77dde139460b0a0ce6b42efa65a1738a275
dizafebriyan <diza.febriyan@gmail.com>
2023-05-22 08:50:11 +0700

    pembaharuan hari 4

---
+++ mainproject/js/blog.js
@@ -0,0 +1,20 @@
+
+let datakita = {
+    nama:"diza febriyan",
+    umur:23,
+    asal:"palembang",
+};
+
+console.log(datakita);
+
+console.log(datakita.nama);
+
+//array of object
+
+let kumpulannama=[
+    {nama: "diza febriyan",umur: 23,},
+    {nama: "ucok", umur:22},
+];
+
+console.log(kumpulannama);
+console.log(kumpulannama[1].nama);
