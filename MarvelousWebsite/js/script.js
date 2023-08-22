8c08102156832e7bd37e15b550b957aefb77ec69
dizafebriyan <diza.febriyan@gmail.com>
2023-08-22 15:50:12 +0700

    marvelousproject

---
+++ MarvelousWebsite/js/script.js
@@ -0,0 +1,18 @@
+document.getElementById("intermediateclass").addEventListener("click", function() {
+    var whatsappURL = "https://wa.me/6281234442872/?text=Excuseme Sir, can i know program marvelous in the Intermediate Class";
+    
+    window.open(whatsappURL, "_blank");
+});
+
+
+document.getElementById("basicclass").addEventListener("click", function() {
+    var whatsappURL = "https://wa.me/6281234442872/?text=Excuseme Sir, can i know program marvelous in the Intermediate Class";
+    
+    window.open(whatsappURL, "_blank");
+});
+
+document.getElementById("businessclass").addEventListener("click", function() {
+    var whatsappURL = "https://wa.me/6281234442872/?text=Excuseme Sir, can i know program marvelous in the Intermediate Class";
+    
+    window.open(whatsappURL, "_blank");
+});
