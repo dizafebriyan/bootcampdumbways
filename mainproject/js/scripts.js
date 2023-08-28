c9f0011627b74299a821ce5caeeb1445f90b83b3
dizafebriyan <diza.febriyan@gmail.com>
2023-08-28 09:54:26 +0700

    style slide slick

--- mainproject/js/scripts.js
+++ mainproject/js/scripts.js
@@ -1,19 +1,10 @@
 // script.js
 
-const map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-}).addTo(map);
-
-const bounds = L.latLngBounds([51.49, -0.16], [51.52, -0.06]);
-map.setMaxBounds(bounds);
-
-const marker = L.marker([51.5, -0.09]).addTo(map);
-marker.bindPopup("Ini adalah lokasi yang bisa di klik.").openPopup();
-
-function onMarkerClick(e) {
-  alert("Anda mengklik marker pada koordinat: " + e.latlng.toString());
-}
-
-marker.on('click', onMarkerClick);
+$(document).ready(function(){
+  $('.format').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    arrows: false
+  });
+});
