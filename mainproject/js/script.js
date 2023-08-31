b91b671e6609015c144cf1a1fc280523ec574018
dizafebriyan <diza.febriyan@gmail.com>
2023-08-31 07:37:19 +0700

    course

--- mainproject/js/script.js
+++ mainproject/js/script.js
@@ -1,18 +1,36 @@
 document.getElementById("intermediateclass").addEventListener("click", function() {
-    var whatsappURL = "https://wa.me/6281234442872/?text=Excuseme Sir, can i know program marvelous in the Intermediate Class";
+    var whatsappURL = "https://wa.me/6281234442872/?text=Excuse me Sir, can i know and register to program marvelous in the Executive Class Level 1 ?";
     
     window.open(whatsappURL, "_blank");
 });
 
 
 document.getElementById("basicclass").addEventListener("click", function() {
-    var whatsappURL = "https://wa.me/6281234442872/?text=Excuseme Sir, can i know program marvelous in the Intermediate Class";
+    var whatsappURL = "https://wa.me/6281234442872/?text=Excuse me Sir, can i know and register to program MARVELOUS in the Executive Class Level 2 ?";
     
     window.open(whatsappURL, "_blank");
 });
 
-document.getElementById("businessclass").addEventListener("click", function() {
-    var whatsappURL = "https://wa.me/6281234442872/?text=Excuseme Sir, can i know program marvelous in the Intermediate Class";
+document.getElementById("expertclass").addEventListener("click", function() {
+    var whatsappURL = "https://wa.me/6281234442872/?text=Excuse me Sir, can i know and register to program MARVELOUS in the Executive Class Level 3 ?";
+    
+    window.open(whatsappURL, "_blank");
+});
+
+document.getElementById("holidayclass").addEventListener("click", function() {
+    var whatsappURL = "https://wa.me/6281234442872/?text=Excuse me Sir, can i know and register to program MARVELOUS in the Holiday Class ?";
+    
+    window.open(whatsappURL, "_blank");
+});
+
+document.getElementById("toeflclass").addEventListener("click", function() {
+    var whatsappURL = "https://wa.me/6281234442872/?text=Excuse me Sir, can i know and register to program MARVELOUS in the TOEFL Class ?";
+    
+    window.open(whatsappURL, "_blank");
+});
+
+document.getElementById("ieltsclass").addEventListener("click", function() {
+    var whatsappURL = "https://wa.me/6281234442872/?text=Excuse me Sir, can i know and register to program MARVELOUS in the IELTS Class ?";
     
     window.open(whatsappURL, "_blank");
 });
